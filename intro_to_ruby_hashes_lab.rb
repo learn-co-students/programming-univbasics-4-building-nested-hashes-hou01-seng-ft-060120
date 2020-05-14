4ab3828ab3b833f6267bcd414677b360c82124f8
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 19:46:09 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  {:railroads => {}}
 
 end
 
@@ -9,7 +10,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  new_hash = base_hash
+  new_hash[:railroads][:pieces] = 4
+  new_hash[:railroads][:rent_in_dollars] = {}
+  new_hash[:railroads][:names] = {}
+  new_hash
 end
 
 def monopoly_with_third_tier
@@ -17,7 +22,17 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  new_hash = monopoly_with_second_tier
+  new_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  new_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  new_hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  new_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
 
+  new_hash[:railroads][:names][:reading_railroad] = {}
+  new_hash[:railroads][:names][:pennsylvania_railroad] = {}
+  new_hash[:railroads][:names][:b_and_o_railroad] = {}
+  new_hash[:railroads][:names][:shortline_railroad] = {}
+  new_hash
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +40,10 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  new_hash = monopoly_with_third_tier
+  new_hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  new_hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
+  new_hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
+  new_hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
+  new_hash
 end
